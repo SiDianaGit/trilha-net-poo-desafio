f71026ad9f1254c9223b8981ed5e19657caee0f9
Simone Alves Diana <si_ap@hotmail.com>
2024-10-04 11:05:59 -0300

    Implementação do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("1199998888","Nokia 3310","XPTO1234",64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+Iphone iphone = new Iphone("1199997777","Iphone16","ABC123456",128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
